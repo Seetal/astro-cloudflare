f875897afc7337f1c7cb141d9a3e3ed7f23f66cf
seetal jhamut <seetal.jhamut@intechnica.co.uk>
2024-09-26 20:46:05 +0100

    endpoint test

--- src/pages/test.json.js
+++ src/pages/test.json.js
@@ -1,4 +1,4 @@
-export async function get() {
+export const GET = async ({ request }) => {
   return new Response(JSON.stringify({message: 'Hello'}), {
     status: 200,
     headers: { 'content-type': 'application/json'},
